bf87cb1031acaef178437483da14dd2bceff03ab
OUCHEN1024 <qaxww88924@gmail.com>
2025-05-16 14:59:49 +0800

    Update url.js

--- url.js
+++ url.js
@@ -1,6 +1,6 @@
 /*
  * 將要隨機派發的網址放入下方（不需排序）
- *
+ *'https://www.surveycake.com/s/x0gn0','https://www.surveycake.com/s/VG00n','https://www.surveycake.com/s/eGQ4q'
  * 並請注意：
  * 1. 網址請用引號（或稱「撇號」，單引號或雙引號皆可）包起來
  * 2. 包起來的網址之間用逗號分隔
